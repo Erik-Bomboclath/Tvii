85e95bdee9ac32778f1ce16a94c790a8f1a0d6a0
Erik-Bomboclath <Bomboclath048@gmail.com>
2025-02-01 17:49:53 +0100

    Delete Tvii/RosePatcher directory

--- Tvii/RosePatcher/README.md
+++
@@ -1,3 +0,0 @@
-# Modified RosePatcher
-do not use this one
-use the one from their official github https://github.com/Project-Rose/RosePatcher
--- Tvii/RosePatcher/src/patches/ssl.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <coreinit/dynload.h>
-
-#include "../utils/logger.h"
-
-namespace patches::ssl {
-    void addCertificateToWebKit();
-}; // namespace ssl
